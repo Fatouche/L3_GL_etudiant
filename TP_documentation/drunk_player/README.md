14d6e6302f0fe34f7991d8336d95e48e14717667
fatouche <foucaud.giovanny@gmail.com>
2017-03-28 10:16:18 +0200

    commentaire doxygen

---
+++ TP_documentation/drunk_player/README.md
@@ -0,0 +1,3 @@
+Description
+SALUT CA farte !? 
+wé mec
--- TP_documentation/drunk_player/src/mainpage.hpp
+++ TP_documentation/drunk_player/src/mainpage.hpp
@@ -1,2 +1,4 @@
 /// \mainpage Documentation de drunk_player
-///
+/// Yop Voila la dooooooc ZAMEL
+/// PUTAIN DE FUCKING DOC DE MERDE !
+/// la voila bordel de cul de chiasse 
